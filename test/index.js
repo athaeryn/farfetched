3c30c435bc6f0f10940b2bb973e3a2c08d97f953
Mike Anderson <hello@mrmikea.com>
2015-01-29 17:55:58 -0600

    Set up some setup

---
+++ test/index.js
@@ -0,0 +1,11 @@
+var assert = require("assert"),
+    farfetched = require("../lib");
+
+
+describe("farfetched", function() {
+  it("should create handlers");
+});
+
+describe("farfetched.clear", function() {
+  it("should clear the handler");
+});
