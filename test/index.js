5e9067e1bb1cef6187be069b309f76ac964246af
Mike Anderson <hello@mrmikea.com>
2015-01-30 17:44:40 -0600

    Support for basic dynamic responses
    These will be more useful after the next feature: regex urls!

--- test/index.js
+++ test/index.js
@@ -28,9 +28,25 @@ describe("farfetched", function() {
       .then(function(response) {
         assert(response === "foo");
         done();
-      });
+      })
+      .catch(done);
   });
 
+
+  it("should allow dynamic responses", function(done) {
+    farfetched("/dynamic", {
+      response: function(url) {
+        return "dynamic response: " + url;
+      }
+    });
+    window.fetch("/dynamic")
+      .then(text)
+      .then(function(response) {
+        assert(response === "dynamic response: /dynamic");
+        done();
+      })
+      .catch(done);
+  });
 
   it("should allow fetches to be mocked more than once", function(done) {
     var a, b;
@@ -44,7 +60,6 @@ describe("farfetched", function() {
       .then(text)
       .then(function(text) { b = text; })
       .then(function() {
-        console.log(a, b);
         assert(a === b);
         done();
       })
@@ -52,23 +67,22 @@ describe("farfetched", function() {
   });
 
 
-  describe("JSON", function() {
-    it("should have a .json method on the response", function(done) {
-      farfetched("/json", {
-        response: { foo: "foo"}
-      });
-
-      window.fetch("/json")
-        .then(json)
-        .then(function(json) {
-          assert(json.foo === "foo");
-          done();
-        });
+  it("should have a .json method on the response", function(done) {
+    farfetched("/json", {
+      response: { foo: "foo"}
     });
+
+    window.fetch("/json")
+      .then(json)
+      .then(function(json) {
+        assert(json.foo === "foo");
+        done();
+      })
+      .catch(done);
   });
 
 
-  it("should let requests through with no matching handlers", function(done) {
+  it("shouldn't intercept requests that aren't mocked", function(done) {
     window.fetch("/bar").then(function(response) {
       ensureNoIntercept(response, done);
     });
